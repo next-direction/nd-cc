82a406c39d129d8ee417b45dea7bf6730860e5cf
Manuel Hesselbarth <jumper85@email.de>
2019-12-31 11:16:04 +0100

    Add links to footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -3,6 +3,17 @@
     <section class="copy">
       &copy 2019 - {{new Date().getFullYear()}} CC - Next Direction
     </section>
+    <section class="links">
+      <a href="https://github.com/next-direction/nd-cc" target="_blank" title="GitHub Repository for this page">
+        <fa :icon="fa.faGithubSquare"/>
+      </a>
+      <a href="https://next-direction.de" target="_blank" title="Next Direction Blog">
+        <fa :icon="fa.faPenSquare"/>
+      </a>
+      <a href="mailto: info@next-direction.de" title="Contact us">
+        <fa :icon="fa.faEnvelopeSquare"/>
+      </a>
+    </section>
     <section class="legal">
       <nuxt-link to="/legal/imprint">Imprint</nuxt-link> &bull;
       <nuxt-link to="/legal/privacy">Privacy</nuxt-link> &bull;
@@ -12,16 +23,30 @@
 </template>
 
 <script>
-    export default {};
+    import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+    import { faEnvelopeSquare, faPenSquare } from '@fortawesome/free-solid-svg-icons';
+
+    export default {
+        computed: {
+            fa () {
+                return {
+                    faEnvelopeSquare,
+                    faGithubSquare,
+                    faPenSquare,
+                };
+            },
+        },
+    };
 </script>
 
 <style lang="scss" scoped>
   footer {
     display: flex;
     justify-content: space-between;
+    align-items: center;
     border-top: 2px solid $secondary-color;
     font-size: 1.1rem;
-    padding: 1.4rem;
+    padding: 1.2rem;
     box-shadow: 0 3px 6px rgba(0, 0, 0, .4) inset;
     position: sticky;
     bottom: 0;
@@ -30,6 +55,15 @@
 
     .copy {
       color: $secondary-color;
+    }
+
+    .links {
+      font-size: 1.4rem;
+
+      a {
+        margin: 0 0.4rem;
+        color: $secondary-color;
+      }
     }
 
     .legal {
